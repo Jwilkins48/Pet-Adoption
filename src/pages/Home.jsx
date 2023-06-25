9018094b63525c171f180405c130dbef17c91e8f
Jenna <jenna48071@gmail.com>
2023-06-25 17:14:08 -0500

    imgs size update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,28 +30,28 @@ function Home() {
             onClick={() => onClick("dog")}
             className="customPink hover:shadow-xl shadow-md animate__animated animate__fadeInDown one card w-[9rem] badge badge-outline badge-secondary h-[8rem] justify-center items-center"
           >
-            <img className="h-20 mb-2" src={dog} alt="dog" />
+            <img className="h-20 w-auto mb-2" src={dog} alt="dog" />
             Dogs
           </button>
           <button
             onClick={() => onClick("cat")}
             className="customBlue hover:shadow-xl shadow-md animate__animated animate__fadeInDown two card w-[9rem] badge badge-outline badge-primary h-[8rem] justify-center items-center"
           >
-            <img className="h-20 mb-2" src={happy} alt="dog" />
+            <img className="h-20 w-auto mb-2" src={happy} alt="dog" />
             Cats
           </button>
           <button
             onClick={() => onClick("horse")}
             className="customPink hover:shadow-xl shadow-md animate__animated animate__fadeInDown three card w-[9rem] badge badge-outline badge-secondary h-[8rem] justify-center items-center"
           >
-            <img className="h-20 mb-2" src={horse} alt="dog" />
+            <img className="h-20 w-auto mb-2" src={horse} alt="dog" />
             Horses
           </button>
           <button
             onClick={() => onClick("rabbit")}
             className="customBlue hover:shadow-xl shadow-md animate__animated animate__fadeInDown four card w-[9rem] badge badge-outline badge-primary h-[8rem] justify-center items-center"
           >
-            <img className="h-20 mb-2" src={rabbit} alt="dog" />
+            <img className="h-20 w-auto mb-2" src={rabbit} alt="dog" />
             Rabbits
           </button>
         </div>
